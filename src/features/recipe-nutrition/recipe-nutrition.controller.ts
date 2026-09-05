7c389956e119c41cb192794c3c38b6dbc985cec9
neel-1905 <neelnarwadkar@gmail.com>
2026-09-06 00:37:26 +0530

    error fixes

--- src/features/recipe-nutrition/recipe-nutrition.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { RecipeNutritionService } from './recipe-nutrition.service';
-
-@Controller('recipe-nutrition')
-export class RecipeNutritionController {
-  constructor(private readonly recipeNutritionService: RecipeNutritionService) {}
-}
--- src/features/recipe-nutrition/recipe-nutrition.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RecipeNutritionService } from './recipe-nutrition.service';
-import { RecipeNutritionController } from './recipe-nutrition.controller';
-
-@Module({
-  controllers: [RecipeNutritionController],
-  providers: [RecipeNutritionService],
-})
-export class RecipeNutritionModule {}
